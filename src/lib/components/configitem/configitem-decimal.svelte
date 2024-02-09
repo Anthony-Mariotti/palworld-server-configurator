756ce55a35481244fcfe1b4b5eddbbfb7019a3c0
Anthony Mariotti <anthony.mariotti@yahoo.com>
2024-02-08 21:24:37 -0500

    Adjust notice to use component

--- src/lib/components/configitem/configitem-decimal.svelte
+++ src/lib/components/configitem/configitem-decimal.svelte
@@ -3,6 +3,8 @@
   import { ConfigItemReset } from '.';
   import type { DecimalProps } from '.';
   import { Slider } from '../ui/slider';
+  import * as Notice from '../notice';
+  import { InfoIcon } from 'lucide-svelte';
 
   type $$Props = DecimalProps;
 
@@ -11,7 +13,7 @@
 </script>
 
 <div
-  class="flex flex-col gap-y-4 bg-gray-50 p-4 lg:grid lg:grid-cols-[1fr_min-content_24rem_min-content] lg:gap-x-10 lg:gap-y-0 xl:grid-cols-[1fr_min-content_36rem_min-content]"
+  class="flex flex-col gap-y-4 bg-gray-50 p-4 lg:grid lg:grid-cols-[1fr_min-content_24rem_min-content] lg:gap-x-10 lg:gap-y-2 xl:grid-cols-[1fr_min-content_36rem_min-content]"
 >
   <div class="flex items-center">
     <label for={key} class="flex text-base font-semibold lg:text-lg">
@@ -39,10 +41,9 @@
     <div class="hidden w-10 lg:block" />
   {/if}
   {#if item.notice}
-    <div
-      class="col-start-3 rounded-sm border-l-4 border-l-yellow-300 bg-yellow-300/10 pl-2 text-yellow-600 lg:mt-2"
-    >
-      {item.notice}
-    </div>
+    <Notice.Root variant="warning" class="col-start-3">
+      <InfoIcon class="h-4 w-4" />
+      <Notice.Title>{item.notice}</Notice.Title>
+    </Notice.Root>
   {/if}
 </div>
--- src/lib/components/configitem/configitem-optional-string.svelte
+++ src/lib/components/configitem/configitem-optional-string.svelte
@@ -3,6 +3,8 @@
   import { ConfigItemReset } from '.';
   import type { OptionalStringProps } from '.';
   import { Input } from '../ui/input';
+  import * as Notice from '../notice';
+  import { InfoIcon } from 'lucide-svelte';
 
   type $$Props = OptionalStringProps;
 
@@ -11,7 +13,7 @@
 </script>
 
 <div
-  class="flex flex-col gap-y-4 bg-gray-50 p-4 lg:grid lg:grid-cols-[1fr_min-content_24rem_min-content] lg:gap-x-10 lg:gap-y-0 xl:grid-cols-[1fr_min-content_36rem_min-content]"
+  class="flex flex-col gap-y-4 bg-gray-50 p-4 lg:grid lg:grid-cols-[1fr_min-content_24rem_min-content] lg:gap-x-10 lg:gap-y-2 xl:grid-cols-[1fr_min-content_36rem_min-content]"
 >
   <div class="col-span-2 flex items-center">
     <label for={key} class="text-base font-semibold lg:text-lg">{key}</label>
@@ -31,10 +33,9 @@
     <div class="hidden w-10 lg:block" />
   {/if}
   {#if item.notice}
-    <div
-      class="col-start-3 rounded-sm border-l-4 border-l-yellow-300 bg-yellow-300/10 pl-2 text-yellow-600 lg:mt-2"
-    >
-      {item.notice}
-    </div>
+    <Notice.Root variant="warning" class="col-start-3">
+      <InfoIcon class="h-4 w-4" />
+      <Notice.Title>{item.notice}</Notice.Title>
+    </Notice.Root>
   {/if}
 </div>
